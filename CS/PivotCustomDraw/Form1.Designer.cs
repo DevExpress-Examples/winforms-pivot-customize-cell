4f96afd1accabb1335f60e158c6c292ebc74d68c
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2022-09-09 18:29:34 +0400

    init repo

---
+++ CS/PivotCustomDraw/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace PivotCustomDraw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo1 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo2 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo3 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo4 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo5 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo6 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo7 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo8 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.FieldInfo fieldInfo9 = new DevExpress.DataAccess.Excel.FieldInfo();
+            DevExpress.DataAccess.Excel.ExcelWorksheetSettings excelWorksheetSettings1 = new DevExpress.DataAccess.Excel.ExcelWorksheetSettings();
+            DevExpress.DataAccess.Excel.ExcelSourceOptions excelSourceOptions1 = new DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1);
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding1 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding2 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding3 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            DevExpress.XtraPivotGrid.DataSourceColumnBinding dataSourceColumnBinding4 = new DevExpress.XtraPivotGrid.DataSourceColumnBinding();
+            this.pivotGridControl1 = new DevExpress.XtraPivotGrid.PivotGridControl();
+            this.excelDataSource1 = new DevExpress.DataAccess.Excel.ExcelDataSource();
+            this.pivotGridField1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField2 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField3 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField4 = new DevExpress.XtraPivotGrid.PivotGridField();
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pivotGridControl1
+            // 
+            this.pivotGridControl1.DataSource = this.excelDataSource1;
+            this.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pivotGridControl1.Fields.AddRange(new DevExpress.XtraPivotGrid.PivotGridField[] {
+            this.pivotGridField1,
+            this.pivotGridField2,
+            this.pivotGridField3,
+            this.pivotGridField4});
+            this.pivotGridControl1.Location = new System.Drawing.Point(0, 0);
+            this.pivotGridControl1.Name = "pivotGridControl1";
+            this.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized;
+            this.pivotGridControl1.Size = new System.Drawing.Size(800, 450);
+            this.pivotGridControl1.TabIndex = 0;
+            this.pivotGridControl1.CustomDrawCell += new DevExpress.XtraPivotGrid.PivotCustomDrawCellEventHandler(this.pivotGridControl1_CustomDrawCell);
+            // 
+            // excelDataSource1
+            // 
+            this.excelDataSource1.FileName = "Data\\Sales.xlsx";
+            this.excelDataSource1.Name = "excelDataSource1";
+            this.excelDataSource1.ResultSchemaSerializable = resources.GetString("excelDataSource1.ResultSchemaSerializable");
+            fieldInfo1.Name = "Category";
+            fieldInfo1.OriginalName = "Category";
+            fieldInfo1.Type = typeof(string);
+            fieldInfo2.Name = "Product";
+            fieldInfo2.OriginalName = "Product";
+            fieldInfo2.Type = typeof(string);
+            fieldInfo3.Name = "State";
+            fieldInfo3.OriginalName = "State";
+            fieldInfo3.Type = typeof(string);
+            fieldInfo4.Name = "UnitsSoldYTD (Sum)";
+            fieldInfo4.OriginalName = "UnitsSoldYTD (Sum)";
+            fieldInfo4.Selected = false;
+            fieldInfo4.Type = typeof(double);
+            fieldInfo5.Name = "UnitsSoldYTDTarget (Sum)";
+            fieldInfo5.OriginalName = "UnitsSoldYTDTarget (Sum)";
+            fieldInfo5.Selected = false;
+            fieldInfo5.Type = typeof(double);
+            fieldInfo6.Name = "RevenueQTD (Sum)";
+            fieldInfo6.OriginalName = "RevenueQTD (Sum)";
+            fieldInfo6.Selected = false;
+            fieldInfo6.Type = typeof(double);
+            fieldInfo7.Name = "RevenueQTDTarget (Sum)";
+            fieldInfo7.OriginalName = "RevenueQTDTarget (Sum)";
+            fieldInfo7.Selected = false;
+            fieldInfo7.Type = typeof(double);
+            fieldInfo8.Name = "RevenueYTD (Sum)";
+            fieldInfo8.OriginalName = "RevenueYTD (Sum)";
+            fieldInfo8.Type = typeof(double);
+            fieldInfo9.Name = "RevenueYTDTarget (Sum)";
+            fieldInfo9.OriginalName = "RevenueYTDTarget (Sum)";
+            fieldInfo9.Selected = false;
+            fieldInfo9.Type = typeof(double);
+            this.excelDataSource1.Schema.AddRange(new DevExpress.DataAccess.Excel.FieldInfo[] {
+            fieldInfo1,
+            fieldInfo2,
+            fieldInfo3,
+            fieldInfo4,
+            fieldInfo5,
+            fieldInfo6,
+            fieldInfo7,
+            fieldInfo8,
+            fieldInfo9});
+            excelWorksheetSettings1.CellRange = null;
+            excelWorksheetSettings1.WorksheetIndex = null;
+            excelWorksheetSettings1.WorksheetName = "Sheet1";
+            excelSourceOptions1.ImportSettings = excelWorksheetSettings1;
+            this.excelDataSource1.SourceOptions = excelSourceOptions1;
+            // 
+            // pivotGridField1
+            // 
+            this.pivotGridField1.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
+            this.pivotGridField1.AreaIndex = 0;
+            this.pivotGridField1.Caption = "Category";
+            dataSourceColumnBinding1.ColumnName = "Category";
+            this.pivotGridField1.DataBinding = dataSourceColumnBinding1;
+            this.pivotGridField1.Name = "pivotGridField1";
+            // 
+            // pivotGridField2
+            // 
+            this.pivotGridField2.AreaIndex = 0;
+            this.pivotGridField2.Caption = "Product";
+            dataSourceColumnBinding2.ColumnName = "Product";
+            this.pivotGridField2.DataBinding = dataSourceColumnBinding2;
+            this.pivotGridField2.Name = "pivotGridField2";
+            // 
+            // pivotGridField3
+            // 
+            this.pivotGridField3.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
+            this.pivotGridField3.AreaIndex = 0;
+            this.pivotGridField3.Caption = "State";
+            dataSourceColumnBinding3.ColumnName = "State";
+            this.pivotGridField3.DataBinding = dataSourceColumnBinding3;
+            this.pivotGridField3.Name = "pivotGridField3";
+            // 
+            // pivotGridField4
+            // 
+            this.pivotGridField4.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
+            this.pivotGridField4.AreaIndex = 0;
+            this.pivotGridField4.Caption = "Revenue YTD (Sum)";
+            this.pivotGridField4.CellFormat.FormatString = "c0";
+            this.pivotGridField4.CellFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
+            dataSourceColumnBinding4.ColumnName = "RevenueYTD (Sum)";
+            this.pivotGridField4.DataBinding = dataSourceColumnBinding4;
+            this.pivotGridField4.Name = "pivotGridField4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pivotGridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraPivotGrid.PivotGridControl pivotGridControl1;
+        private DevExpress.DataAccess.Excel.ExcelDataSource excelDataSource1;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField1;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField2;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField3;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField4;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,16 @@
+# Pivot Grid for WinForms - Customize Cells 
+
+
+
+## Files to Look At
+
+
+## Documentation
+
+[Custom Draw](https://docs.devexpress.com/WindowsForms/1817/controls-and-libraries/pivot-grid/appearance/custom-draw)
+
+## More Examples
+
+- [Pivot Grid for WinForms - Highlight Field Headers when they are Filtered](https://github.com/DevExpress-Examples/how-to-highlight-field-headers-when-they-are-filtered-e545)
+- [Pivot Grid for WinForms - Change the Field Value Header Background with the CustomDrawFieldValue Event](https://github.com/DevExpress-Examples/how-to-change-the-field-value-header-appearance-backcolor-or-draw-it-manually-e2809)
+- [Pivot Grid for WinForms - Draw a Custom Element when a User Hovering Over Field Value with a Mouse](https://github.com/DevExpress-Examples/how-to-draw-a-custom-element-when-a-user-hovering-over-field-value-with-a-mouse-e2228)
